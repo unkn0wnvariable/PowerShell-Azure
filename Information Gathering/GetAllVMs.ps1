8b005932f2ba4f95b276ccf83c5cbcba2283b280
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2020-07-10 13:48:41 +0100

    Created GetAllVMs.ps1

---
+++ Information Gathering/GetAllVMs.ps1
@@ -0,0 +1,37 @@
+# Script to get all VMs from Azure
+#
+
+# OPutput file
+$outputPath = 'C:\Temp\AllVMs.csv'
+
+# Import the Az module and connect to Azure
+Import-Module Az
+Connect-AzAccount -UseDeviceAuthentication
+
+# RegEx to find the subscriptions we care about
+$subscriptionRegEx = '^.*$'
+
+# Get all the relevant subscriptions
+$subscriptions = Get-AzSubscription | Where-Object {$_.Name -match $subscriptionRegEx}
+
+# Initialise the variable for results
+$allVMs = @()
+
+# Run through the subscriptions getting all the webapps in them
+foreach ($subscription in $subscriptions) {
+    Write-Output ('Getting VMs from subscription: ' + $subscription.Name)
+    $null = Set-AzContext -SubscriptionObject $subscription
+    $vms = Get-AzVM
+    foreach ($vm in $vms) {
+        $allVMs += [PSCustomObject]@{
+            VMName      = $vm.Name
+            VMRG        = $vm.ResourceGroupName
+            OSType      = $vm.StorageProfile.OsDisk.OsType
+            OSSku       = $vm.StorageProfile.ImageReference.Sku
+            LicenseType = $vm.LicenseType
+        }
+    }
+}
+
+# Output sorted list of all default hostnames for the webapps
+$allVMs | Sort-Object -Property VMName | Export-Csv -NoTypeInformation -Path $outputPath
