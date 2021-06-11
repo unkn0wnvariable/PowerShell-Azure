f8ad091f9c95f6dd3ab9c9b459ed5f5ae431722a
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2021-06-11 11:10:37 +0100

    Create GetAllPublicIPsAndFQDNs.ps1

---
+++ InformationGathering/GetAllPublicIPsAndFQDNs.ps1
@@ -0,0 +1,31 @@
+# Script to get all assigned public IPs and FQDNs for resources in an Azure tenant.
+#
+
+# Import the Az module and connect to Azure
+Import-Module Az
+Connect-AzAccount -UseDeviceAuthentication
+
+# RegEx to find the subscriptions we care about
+$subscriptionRegEx = '^.*$'
+
+# Where to save the results?
+$outputFile = "C:\Temp\PublicIPsAndFQDNs.txt"
+
+# Get all the relevant subscriptions
+$subscriptions = Get-AzSubscription | Where-Object {$_.Name -match $subscriptionRegEx}
+
+# Initialise the variable for results
+$publicIPs = @()
+
+# Run through the subscriptions getting all the public IPs in them
+foreach ($subscription in $subscriptions) {
+    Write-Output ('Getting resources from subscription: ' + $subscription.Name)
+    $null = Set-AzContext -SubscriptionObject $subscription
+    $publicIPs += Get-AzPublicIpAddress | Select-Object Name,IpAddress,PublicIpAllocationMethod,@{Name="Fqdn";Expression={$_.DnsSettings.Fqdn}}
+}
+
+# Output sorted list of all static IP addresses with no FQDN
+($publicIPs | Where-Object {$_.PublicIpAllocationMethod -eq 'Static' -and $_.Fqdn -eq $null}).IpAddress | Sort-Object > $outputFile
+
+# Output sorted list of all FQDN's
+($publicIPs | Where-Object {$_.Fqdn -ne $null}).Fqdn | Sort-Object >> $outputFile
