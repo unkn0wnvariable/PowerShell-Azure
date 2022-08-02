211a719bf2649459b2d821efe818d50fa45a458a
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2022-08-02 15:27:20 +0100

    Added check IP address is assigned

--- InformationGathering/GetAllPublicIPsAndFQDNs.ps1
+++ InformationGathering/GetAllPublicIPsAndFQDNs.ps1
@@ -12,7 +12,7 @@ $subscriptionRegEx = '^.*$'
 $outputFile = "C:\Temp\PublicIPsAndFQDNs.txt"
 
 # Get all the relevant subscriptions
-$subscriptions = Get-AzSubscription | Where-Object {$_.Name -match $subscriptionRegEx}
+$subscriptions = Get-AzSubscription | Where-Object { $_.Name -match $subscriptionRegEx }
 
 # Initialise the variable for results
 $publicIPs = @()
@@ -21,14 +21,14 @@ $publicIPs = @()
 foreach ($subscription in $subscriptions) {
     Write-Output ('Getting resources from subscription: ' + $subscription.Name)
     $null = Set-AzContext -SubscriptionObject $subscription
-    $publicIPs += Get-AzPublicIpAddress | Select-Object Name,IpAddress,PublicIpAllocationMethod,@{Name="Fqdn";Expression={$_.DnsSettings.Fqdn}}
+    $publicIPs += Get-AzPublicIpAddress | Where-Object { $_.IpAddress -ne 'Not Assigned' } | Select-Object Name, IpAddress, PublicIpAllocationMethod, @{Name = "Fqdn"; Expression = { $_.DnsSettings.Fqdn } }
 }
 
 # Output sorted list of all FQDN's to file
-Out-File -FilePath $outputFile -InputObject (($publicIPs | Where-Object {$_.Fqdn -ne $null}).Fqdn | Sort-Object)
+Out-File -FilePath $outputFile -InputObject (($publicIPs | Where-Object { $_.Fqdn -ne $null }).Fqdn | Sort-Object)
 
 # Add sorted list of all static IP addresses with no FQDN to output file
-Out-File -FilePath $outputFile -InputObject (($publicIPs | Where-Object {$_.PublicIpAllocationMethod -eq 'Static' -and $_.Fqdn -eq $null}).IpAddress | Sort-Object) -Append
+Out-File -FilePath $outputFile -InputObject (($publicIPs | Where-Object { $_.PublicIpAllocationMethod -eq 'Static' -and $_.Fqdn -eq $null }).IpAddress | Sort-Object) -Append
 
 # Disconnect from Azure
 Disconnect-AzAccount
