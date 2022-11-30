36da039a9dd7242b75a72922b0d55e6328f6f928
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2022-11-30 10:02:20 +0000

    Update GetAllvNetConnectedIPAddresses.ps1

--- InformationGathering/GetAllvNetConnectedIPAddresses.ps1
+++ InformationGathering/GetAllvNetConnectedIPAddresses.ps1
@@ -1,11 +1,11 @@
 # Script to get all IP addresses connected to all vNets in a set of subscriptions
 #
-# Outputs full details of the results to a set of CSV files plus a basic list of IPs to text files
+# Outputs full details of the results to a CSV file plus a basic list of IPs to a text file for each subscription
 #
 
 # Where to create the output files?
 $outputFilesPath = 'C:\Temp\'
-$outputFilesPrefix = 'vNetPrivateIPAddresses_'
+$outputFilesPrefix = 'vNetPrivateIPAddresses'
 
 # RegEx to find the subscriptions we care about
 $subscriptionRegEx = '^.*$'
@@ -17,6 +17,9 @@ Connect-AzAccount
 # Get all the relevant subscriptions
 $subscriptions = Get-AzSubscription | Where-Object { $_.Name -match $subscriptionRegEx }
 
+# Create output list for full details
+$allVNetConnectedIPs = @()
+
 # Run through each subscription in the retreived list
 foreach ($subscription in $subscriptions) {
 
@@ -24,6 +27,7 @@ foreach ($subscription in $subscriptions) {
     $vNetConnectedIPs = @()
 
     # Select the subscription to run commands against
+    Write-Output ('Getting resources from subscription: ' + $subscription.Name)
     $null = Select-AzSubscription -SubscriptionObject $subscription
 
     # Get all virtual networks from the selected subscription
@@ -40,12 +44,13 @@ foreach ($subscription in $subscriptions) {
             foreach ($ipConfiguration in $subnet.ipConfigurations) {
                 if ($ipConfiguration.PrivateIpAddress.Length -gt 0) {
                     $vNetConnectedIPs += [PSCustomObject]@{
-                        PrivateIpAddress = $ipConfiguration.PrivateIpAddress;
-                        IPAddressName    = $ipConfiguration.Id.Split('/')[-1];
-                        ResourceName     = $ipConfiguration.Id.Split('/')[8];
-                        ResourceGroup    = $ipConfiguration.Id.Split('/')[4];
-                        Provider         = $ipConfiguration.Id.Split('/')[6];
-                        ResourceType     = $ipConfiguration.Id.Split('/')[7];
+                        'Subscription'     = $subscription.Name;
+                        'PrivateIpAddress' = $ipConfiguration.PrivateIpAddress;
+                        'IPAddressName'    = $ipConfiguration.Id.Split('/')[-1];
+                        'ResourceName'     = $ipConfiguration.Id.Split('/')[8];
+                        'ResourceGroup'    = $ipConfiguration.Id.Split('/')[4];
+                        'Provider'         = $ipConfiguration.Id.Split('/')[6];
+                        'ResourceType'     = $ipConfiguration.Id.Split('/')[7];
                     }
                 }
             }
@@ -53,14 +58,17 @@ foreach ($subscription in $subscriptions) {
         
     }
 
-    # Output Full details to CSV a file
-    $outputCsvFileSubscription = $outputFilesPath + $outputFilesPrefix + $subscription.Name + '.csv'
-    $vNetConnectedIPs | Export-Csv -Path $outputCsvFileSubscription -NoTypeInformation
+    # Add results from current subscription to the full details list
+    $allVNetConnectedIPs += $vNetConnectedIPs
 
     # Output just the list of IP addresses to a txt file
-    $outputTxtFileSubscription = $outputFilesPath + $outputFilesPrefix + $subscription.Name + '.txt'
+    $outputTxtFileSubscription = $outputFilesPath + $outputFilesPrefix + '_' + $subscription.Name + '.txt'
     $vNetConnectedIPs.PrivateIpAddress | Out-File -FilePath $outputTxtFileSubscription
 }
+
+# Output Full details to CSV a file
+$outputCsvFileSubscription = $outputFilesPath + $outputFilesPrefix + '.csv'
+$allVNetConnectedIPs | Export-Csv -Path $outputCsvFileSubscription -NoTypeInformation
 
 # Disconnect from Azure
 Disconnect-AzAccount
