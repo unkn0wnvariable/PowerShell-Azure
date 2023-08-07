0947683c44b0d19f01fb4dca6ade1c9f86a44389
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2023-08-07 11:21:54 +0100

    Update GetAllWebApps.ps1

--- InformationGathering/GetAllWebApps.ps1
+++ InformationGathering/GetAllWebApps.ps1
@@ -1,15 +1,15 @@
 # Script to get all enabled webapp default urls from Azure
 #
 
-# Import the Az module and connect to Azure
-Import-Module Az.Accounts, Az.Websites
-Connect-AzAccount
+# Where to save the results?
+$outputFile = "C:\Temp\WebApps.txt"
 
 # RegEx to find the subscriptions we care about
 $subscriptionRegEx = '^.*$'
 
-# Where to save the results?
-$outputFile = "C:\Temp\WebApps.txt"
+# Import required Az modules and connect to Azure
+Import-Module Az.Accounts, Az.Websites
+Connect-AzAccount
 
 # Get all the relevant subscriptions
 $subscriptions = Get-AzSubscription | Where-Object { $_.Name -match $subscriptionRegEx }
