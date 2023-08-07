73382d1e1ac78148e3bcf10dbee6fe7d6fad24f3
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2023-08-07 11:21:39 +0100

    Update GetAllVMsDetailed.ps1

--- InformationGathering/GetAllVMsDetailed.ps1
+++ InformationGathering/GetAllVMsDetailed.ps1
@@ -4,12 +4,12 @@
 # OPutput file
 $outputPath = 'C:\Temp\AllVMsDetailed.csv'
 
-# Import the Az module and connect to Azure
-Import-Module Az
-Connect-AzAccount
-
 # RegEx to find the subscriptions we care about
 $subscriptionRegEx = '^.*$'
+
+# Import required Az modules and connect to Azure
+Import-Module Az.Accounts, Az.Compute
+Connect-AzAccount
 
 # Get all the relevant subscriptions
 $subscriptions = Get-AzSubscription | Where-Object {$_.Name -match $subscriptionRegEx}
@@ -41,7 +41,7 @@ foreach ($subscription in $subscriptions) {
     }
 }
 
-# Output sorted list of all default hostnames for the webapps
+# Output sorted list of all VM details
 $allVMsDetailed | Sort-Object -Property VMName | Export-Csv -NoTypeInformation -Path $outputPath
 
 # Disconnect from Azure
